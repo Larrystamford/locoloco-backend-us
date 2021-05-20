bd706872f8299a3c692a059ab71365293fad1885
Larry <larrylee3107@gmail.com>
2021-05-21 00:21:12 +0800

    120 videos

--- controllers/download-tiktoks.js
+++ controllers/download-tiktoks.js
@@ -168,7 +168,7 @@ let DownloadTiktoksController = {
           options.number = counter;
         }
       } else {
-        options.number = 50;
+        options.number = 120;
       }
 
       if (options.number >= 0) {
